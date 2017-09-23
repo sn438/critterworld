e166fa58cf171728212f8b39a5654485f796d41c
Travis Westura <twestura@gmail.com>
2017-09-23 18:59:35 -0400

    Add example test

---
+++ src/test/java/parsertests/ParserTest.java
@@ -0,0 +1,34 @@
+package parsertests;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.junit.Test;
+
+import ast.Program;
+import parse.Parser;
+import parse.ParserFactory;
+
+/**
+ * This class contains tests for the Critter parser.
+ */
+public class ParserTest {
+
+    /** Checks that a valid critter program is not {@code null} when parsed. */
+    @Test
+    public void testProgramIsNotNull() {
+        InputStream in = ParserTest.class.getResourceAsStream("draw_critter.txt");
+        assertNotNull(in);
+        Reader r = new BufferedReader(new InputStreamReader(in));
+        Parser p = ParserFactory.getParser();
+        Program prog = p.parse(r);
+        assertNotNull("A valid critter program should not be null.", prog);
+    }
+
+    // TODO continue adding tests
+
+}
