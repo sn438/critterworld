d82ed5429c812f117f6140060fc55498975f3a37
sn438 <31802007+sn438@users.noreply.github.com>
2017-10-07 21:11:25 -0400

    UnaryExpr

--- src/main/java/ast/UnaryExpr.java
+++ src/main/java/ast/UnaryExpr.java
@@ -2,8 +2,14 @@ package ast;
 
 public class UnaryExpr extends AbstractNode implements Expr
 {
-
 	
+	private Factor factor;
+	private Expr expression;
+	
+	public UnaryExpr(Factor factor, Expr expression) {
+		this.factor = factor;
+		this.expression = expression;
+	}
 	
 	@Override
 	public int evaluateNode()
@@ -12,5 +18,7 @@ public class UnaryExpr extends AbstractNode implements Expr
 		return 0;
 	}
 	
-	public enum
+	public enum Factor{
+		CONSTANT, MEMORY, EXPRESSION, NEGATE, SENSOR
+	}
 }
