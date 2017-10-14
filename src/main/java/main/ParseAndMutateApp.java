d8c3aa8b584ede7a25e30f0ce44137271248f632
Jacob Glueck <swimgiraffe435@gmail.com>
2017-10-14 07:48:38 -0400

    Revert "Remove a4 main class"
    This reverts commit 620c7a43d207103c789c3fb1c4c2290bae429a95.

---
+++ src/main/java/main/ParseAndMutateApp.java
@@ -0,0 +1,39 @@
+package main;
+
+public class ParseAndMutateApp {
+
+	public static void main(String[] args) {
+		int n = 0;
+		String file;
+		try {
+			if (args.length == 1) {
+				file = args[0];
+			} else if (args.length == 3 && args[0].equals("--mutate")) {
+				n = parsePositive(args[1]);
+				file = args[2];
+			} else {
+				throw new IllegalArgumentException();
+			}
+			// TODO print program and mutations
+		} catch (IllegalArgumentException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Usage:\n" + "  <input_file>\n" + " --mutate <n> <input_file");
+		}
+	}
+
+	/**
+     * Parses {@code str} to an integer.
+     * 
+     * @param str
+     *            the string to parse
+     * @return the integer represented by {@code str}
+     * @throws NumberFormatException
+     *             if {@code str} does not contain a parsable integer
+     * @throws IllegalArgumentException
+     *             if {@code str} represents a negative integer
+     */
+	public static int parsePositive(String str) {
+		int n = Integer.parseInt(str);
+		if (n < 0) { throw new IllegalArgumentException(); }
+		else { return n; }
+	}
+}
