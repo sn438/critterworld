1a1c3d3553c93eb032c45404eb36f35c72c7a24f
Andy <az389@cornell.edu>
2017-10-16 23:01:09 -0400

    Adding print statements

--- src/main/java/ast/MutationRemove.java
+++ src/main/java/ast/MutationRemove.java
@@ -13,15 +13,17 @@ public class MutationRemove extends AbstractMutation
 		return (m instanceof MutationRemove);
 	}
 
+	@Override
 	public boolean mutate(Rule r)
 	{
 		Node parent = r.getParent();
 		parent.replaceChild(r, null);
 		if(printMutationDetail)
-			System.out.println("Removed the Rule node\n" + r + "\n");
+			System.out.println("Removed the Rule node " + r.toString());
 		return true;
 	}
 	
+	@Override
 	public boolean mutate(BinaryCondition c)
 	{
 		Node parent = c.getParent();
@@ -30,24 +32,23 @@ public class MutationRemove extends AbstractMutation
 			return false;
 		parent.replaceChild(c, replacement);
 		if(printMutationDetail)
-			System.out.println("Removed the BincaryCondition node\n" + c + "\n");
+			System.out.println("Removed the binary condition node " + c.toString());
 		return true;
 	}
 	
+	@Override
 	public boolean mutate(Update u)
 	{
 		Command parent = (Command) u.getParent();
 		if(parent.getUpdateList().size() == 0)
 			return false;
 		parent.replaceChild(u, null);
-		if (printMutationDetail) {
-			System.out.println("After mutating: " + parent);
-		}
 		if(printMutationDetail)
-			System.out.println("Removed the Update node\n" + u + "\n");
+			System.out.println("Removed the Update node " + u.toString());
 		return true;
 	}
 
+	@Override
 	public boolean mutate(Action a)
 	{
 		Command parent = (Command) a.getParent();
@@ -55,10 +56,11 @@ public class MutationRemove extends AbstractMutation
 			return false;
 		parent.replaceChild(a, null);
 		if(printMutationDetail)
-			System.out.println("Removed the Action node\n" + a + "\n");
+			System.out.println("Removed the Action node " + a.toString());
 		return true;
 	}
 	
+	@Override
 	public boolean mutate(Relation r)
 	{
 		Node parent = r.getParent();
@@ -67,10 +69,11 @@ public class MutationRemove extends AbstractMutation
 			return false;
 		parent.replaceChild(r, replacement);
 		if(printMutationDetail)
-			System.out.println("Removed the Relation node\n" + r + "\n");
+			System.out.println("Removed the Relation node " + r.toString());
 		return true;
 	}
-
+	
+	@Override
 	public boolean mutate(BinaryExpr be)
 	{
 		Node parent = be.getParent();
@@ -79,10 +82,11 @@ public class MutationRemove extends AbstractMutation
 			return false;
 		parent.replaceChild(be, replacement);
 		if(printMutationDetail)
-			System.out.println("Removed the BinaryExpr node\n" + be + "\n");
+			System.out.println("Removed the binary expression node " + be.toString());
 		return true;
 	}
 	
+	@Override
 	public boolean mutate(UnaryExpr ue)
 	{
 		Node parent = ue.getParent();
@@ -91,9 +95,10 @@ public class MutationRemove extends AbstractMutation
 			return false;
 		parent.replaceChild(ue, replacement);
 		if(printMutationDetail)
-			System.out.println("Removed the UnaryExpr node\n" + ue + "\n");
+			System.out.println("Removed the unary expression node " + ue.toString());
 		return true;}
 	
+	@Override
 	public boolean mutate(Sensor s)
 	{
 		Node parent = s.getParent();
@@ -102,15 +107,17 @@ public class MutationRemove extends AbstractMutation
 			return false;
 		parent.replaceChild(s, replacement);
 		if(printMutationDetail)
-			System.out.println("Removed the Sensor node\n" + s + "\n");
+			System.out.println("Removed the Sensor node " + s.toString());
 		return true;
 	}
 	
 	//Unsupported methods
+	@Override
 	public boolean mutate(ProgramImpl p)
 	{
 		return false;
 	}
+	@Override
 	public boolean mutate(Command comm)
 	{
 		return false;
