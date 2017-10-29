ccf413096637d20c08578d5a8471d265917e5b91
sn438 <31802007+sn438@users.noreply.github.com>
2017-10-29 15:51:11 -0400

    Revert "Revert "Merge branch 'master' of https://github.coecis.cornell.edu/cs2112-f17/team-az389-sn438-cw""
    This reverts commit 1659d793a583869999bcf2ca34ff03cf3d7a0325.

--- src/main/java/simulation/Test.java
+++ src/main/java/simulation/Test.java
@@ -7,6 +7,10 @@ public class Test
 	public static void main(String[] args) throws FileNotFoundException, IllegalArgumentException
 	{
 		SimpleWorld sw = new World("world.txt");
-		System.out.println(sw.printGrid().toString());
+		System.out.println(sw.printGrid().toString() + "\ncritter #: " + sw.numRemainingCritters());
+		
+		for(int i = 0; i < 1000; i++)
+			sw.advanceOneTimeStep();
+		System.out.println(sw.printGrid().toString() + "\ncritter #: " + sw.numRemainingCritters());
 	}
 }
