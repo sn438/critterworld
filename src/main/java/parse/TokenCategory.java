8fc49a48b7b0d7351fe1b0df63e19cf2fea71d24
sn438 <31802007+sn438@users.noreply.github.com>
2017-10-31 12:39:25 -0400

    Tokenizer Has Been Updated With Correct Solution

--- src/main/java/parse/TokenCategory.java
+++ src/main/java/parse/TokenCategory.java
@@ -1,7 +1,5 @@
 package parse;
 
-/** Type of token representing a terminal in the critter language. */
-public enum TokenCategory
-{
-	ACTION, RELOP, ADDOP, MULOP, SENSOR, MEMSUGAR, OTHER;
+enum TokenCategory {
+    ACTION, RELOP, ADDOP, MULOP, SENSOR, MEMSUGAR, OTHER;
 }
