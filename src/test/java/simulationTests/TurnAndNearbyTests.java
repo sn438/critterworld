cb93ef26d11bd86c1a934f2a1b9d46993bb0cb53
Andy <az389@cornell.edu>
2017-10-31 22:49:26 -0400

    fix merge

--- src/test/java/simulationTests/TurnAndNearbyTests.java
+++ src/test/java/simulationTests/TurnAndNearbyTests.java
@@ -24,11 +24,6 @@ public class TurnAndNearbyTests {
 	/**
 	 * simpleTurn checks to see if a critter can turn right and left properly
 	 */
-<<<<<<< HEAD:src/test/java/simulationTests/TurnTests.java
-=======
-	
-	
->>>>>>> 41fe28e826698ca5cf3e31913bdf54842644b50c:src/test/java/simulationTests/TurnAndNearbyTests.java
 	@Test
 	public void simpleTurn() {
 		System.out.println("simpleTurn");
@@ -36,17 +31,11 @@ public class TurnAndNearbyTests {
 		console1.advanceTime(1);
 		console1.worldInfo();
 	}
-	
-<<<<<<< HEAD:src/test/java/simulationTests/TurnTests.java
-	
-	/**
-	 * turnIfFood tests to see if a critter can turn if there is food right next to it.
-=======
+
 	/**
 	 * turnIfFood tests to see if a critter can turn if there is food right next to it. Nearby was
 	 * also tested by placing the food at different points around the critter and having the critter turn if
 	 * it can sense it. As a result, this test covers both functionalities. 
->>>>>>> 41fe28e826698ca5cf3e31913bdf54842644b50c:src/test/java/simulationTests/TurnAndNearbyTests.java
 	 */
 	
 	@Test
