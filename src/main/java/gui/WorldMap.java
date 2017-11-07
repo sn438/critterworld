6d7e9f565964c2db7a6575abd31610c35b51ca41
Andy <az389@cornell.edu>
2017-11-06 20:39:28 -0500

    GUI fixes

--- src/main/java/gui/WorldMap.java
+++ src/main/java/gui/WorldMap.java
@@ -1,6 +1,15 @@
 package gui;
 
+import javafx.scene.canvas.Canvas;
+
 public class WorldMap
 {
+	private Canvas canvas;
+	private WorldModel world;
 	
+	public WorldMap(Canvas c, WorldModel w)
+	{
+		canvas = c;
+		world = w;
+	}
 }
