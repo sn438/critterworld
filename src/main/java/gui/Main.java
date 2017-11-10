5798bdf4c7ec08b2cee869910530d0bde8d63586
ra534 <ra534@cornell.edu>
2017-11-10 03:33:38 -0500

    todo

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -37,3 +37,7 @@ public class Main extends Application {
 
 // TODO fix zooming + panning position
 // TODO right-click + drag doesn't work for most laptops...
+// TODO fix hex selection
+// TODO make highlighting an outline instead of a fill
+// TODO fix weird lagginess in panning
+// TODO black universe, grey hex grid, grey fully filled hexes as rocks, neon circles for food (maybe yellow), neon isosceles triangles for critters (maybe green + species variation so different shades)
